311f4f78866b115c1b15bd9fd19526267be0552e
whdkfk <joara080827@gmail.com>
2025-07-07 15:59:40 +0900

    refactor(#9) :: 코멘트 반영

--- src/components/ui/post/style.ts
+++ src/components/ui/post/style.ts
@@ -8,4 +8,12 @@ export const PostImg = styled.div`
   height: 40vh;
   position: relative;
   overflow: hidden;
+
+  &:hover::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    transition: background-color 0.3s ease;
+  }
 `
