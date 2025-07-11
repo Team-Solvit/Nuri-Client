c4752dd2eca4c3128036a2475aa5ac3ed1d03cc3
dohun08 <bemik624@gmail.com>
2025-07-11 12:26:06 +0900

    refactor(#13) :: 모달 종료 개선

--- src/components/layout/modal/index.tsx
+++ src/components/layout/modal/index.tsx
@@ -1,19 +1,25 @@
 'use client';
 import * as S from "./style";
-import { useModalStore } from "@/store/modal";
-import { createPortal } from "react-dom";
-import { useEffect, useState } from "react";
+import {useModalStore} from "@/store/modal";
+import {createPortal} from "react-dom";
+import {useEffect, useState} from "react";
+import {useRouter} from "next/navigation";
 
-export default function Modal({ children }: { children: React.ReactNode }) {
-	const { close, isOpen } = useModalStore();
+export default function Modal({children}: { children: React.ReactNode }) {
+	const router = useRouter();
+	const {close, isOpen} = useModalStore();
 	const [mounted, setMounted] = useState(false)
 	useEffect(() => {
 		setMounted(true)
 	}, [])
 	if (!mounted || !isOpen) return null
-
+	
+	const closeModal = () => {
+		close()
+		router.push(window.location.pathname, {scroll: false});
+	}
 	return createPortal(
-		<S.Black onClick={close}>
+		<S.Black onClick={closeModal}>
 			<S.Content onClick={(e) => e.stopPropagation()}>
 				{children}
 			</S.Content>
