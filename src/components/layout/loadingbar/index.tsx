e5996bb0078f845903157e81650b273633b76e13
dohun08 <bemik624@gmail.com>
2025-07-16 10:31:05 +0900

    feat(SOLVIT-25-52) :: 로딩바 시간 최적화

--- src/components/layout/loadingbar/index.tsx
+++ src/components/layout/loadingbar/index.tsx
@@ -1,22 +1,14 @@
 'use client';
 
-import {useEffect, useRef} from 'react';
+import {useEffect} from 'react';
 import {usePathname} from 'next/navigation';
 import NProgress from 'nprogress';
 import "./style.css"
 
 export default function TopLoadingBar() {
 	const pathname = usePathname();
-	const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-	
 	useEffect(() => {
-		timeoutRef.current = setTimeout(() => {
-			NProgress.done();
-		}, 1000);
-		
-		return () => {
-			if (timeoutRef.current) clearTimeout(timeoutRef.current);
-		};
+		NProgress.done();
 	}, [pathname]);
 	
 	return null;
