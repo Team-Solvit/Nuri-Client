3f30619f5a4ffc916b8b3e4ef7e6b1adcc5fe502
dohun08 <bemik624@gmail.com>
2025-07-30 14:40:59 +0900

    feat/SOLVIT25-67 :: 반응형 셋팅 추가

---
+++ src/styles/media.ts
@@ -0,0 +1,19 @@
+export const breakpoints = {
+	mobile: 393,
+	desktop: 1024,
+};
+
+export const mq = {
+	mobile: `@media (max-width: ${breakpoints.mobile}px)`,
+};
+
+// 사용예시
+// import { mq } from '@/styles/media';
+//
+// const boxStyle = styled.p`
+//   font-size: 2rem;
+//
+//   ${mq.mobile} {
+//     font-size: 1.6rem;
+//   }
+// `;
