482096c8d8366b1d4a60f588ac2f166a1d07362a
dohun08 <bemik624@gmail.com>
2025-08-02 16:02:56 +0900

    feat/SOLVIT25-71 :: 홈페이지 네비게이트 퍼블리싱

--- src/components/ui/navigate/style.ts
+++ src/components/ui/navigate/style.ts
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import {colors, fontSizes, radius, zIndex} from "@/styles/theme";
+import {mq} from "@/styles/media";
 
 export const NavigateContainer = styled.section`
   display: inline-flex;
@@ -10,6 +11,19 @@ export const NavigateContainer = styled.section`
   gap: 1rem;
   border-radius: ${radius.md};
   border: 1px solid ${colors.line};
+
+  ${mq.mobile} {
+    justify-content: space-between;
+    flex-direction: row;
+    background: rgba(255, 255, 255, 0.90);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    box-shadow: 0 4.57px 7.312px 0 rgba(0, 0, 0, 0.03), 0 10.967px 36.558px 0 rgba(0, 0, 0, 0.04);
+    backdrop-filter: blur(2px);
+    z-index: ${zIndex.dropdown}
+  }
 `
 export const NavigateBtn = styled.button<{ isActive: boolean }>`
   display: flex;
@@ -24,8 +38,18 @@ export const NavigateBtn = styled.button<{ isActive: boolean }>`
   border-radius: ${radius.md};
   background-color: ${({isActive}) => isActive ? '#efefef' : 'transparent'};
 
+  ${mq.mobile} {
+    width: max-content;
+  }
+
   &:hover {
     background-color: #F8F8F8;
+  }
+
+  & > p {
+    ${mq.mobile} {
+      display: none;
+    }
   }
 `
 export const IconBox = styled.div`
@@ -50,4 +74,28 @@ export const Count = styled.span`
   border-radius: ${radius.full};
   background-color: ${colors.primary};
   color: ${colors.background};
+`
+export const Logo = styled.div`
+  display: none;
+
+  ${mq.mobile} {
+    position: relative;
+    width: 3.4rem;
+    height: 3.4rem;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    justify-content: center;
+  }
+`
+export const BtnBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  flex-direction: column;
+
+  ${mq.mobile} {
+    flex-direction: row;
+    gap: 0.2rem;
+  }
 `
