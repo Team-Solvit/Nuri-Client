a0b5fc5ff2e2727e252322c3986ee4fcb1e30b42
zuu3 <ojh27467242@gmail.com>
2025-10-10 17:37:48 +0900

    feat(SOLVIT25-106) :: 아이콘 추가 완료

--- src/utils/facilityIcons.ts
+++ src/utils/facilityIcons.ts
@@ -9,7 +9,29 @@ export const FACILITY_ICON_MAP: { [key: string]: string } = {
 
   '개별 냉장고': 'fridge',
   '개별냉장고': 'fridge',
-  '냉장고': 'fridge'
+  '냉장고': 'fridge',
+
+  '커튼': 'curtain',
+
+  '개별 화장실': 'toilet',
+  '개별화장실': 'toilet',
+  '화장실': 'toilet',
+
+  '책상/의자': 'deskwithchair',
+
+  '개인 세탁기': 'washing-machine',
+  '개인세탁기': 'washing-machine',
+  '세탁기': 'washing-machine',
+
+  '개인 TV': 'tv',
+
+  '전자레인지': 'microwave',
+
+  '개별 에어컨': 'air-conditioner',
+  '개별에어컨': 'air-conditioner',
+  '에어컨': 'air-conditioner',
+
+  '스탠드': 'stand',
 };
 
 export const getFacilityIcon = (name: string): string | undefined => {
