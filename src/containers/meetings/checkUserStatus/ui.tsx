f5f6d588dea6979e7c8c159ed92a2a9a7e3b5691
dohun08 <bemik624@gmail.com>
2025-10-23 06:13:43 +0900

    fix/SOLVIT25-91 :: 헤더 클릭시 이동 불가

--- src/containers/meetings/checkUserStatus/ui.tsx
+++ src/containers/meetings/checkUserStatus/ui.tsx
@@ -8,12 +8,15 @@ import {MeetingQueries} from "@/services/meeting";
 import {Status} from "@/types/meetings";
 import {useSelectOtherMeetingDetailStore} from "@/store/selectOtherMeetingDetail";
 import {useUserStore} from "@/store/user";
+import {useOtherMeetingFind} from "@/store/otherMeetingFind";
+import {useParams} from "next/navigation";
 
 export default function CheckUserStatus({children}: { children: React.ReactNode}) {
 	const {setFree, setEnteringMeeting, setSendRequest} = useIsEnteringMeetingStore()
 	const {setMeetingId} = useSelectOtherMeetingDetailStore()
 	const navigate = useNavigationWithProgress()
 	const {id} = useUserStore()
+	const params = useParams()
 	
 	const { data } = useQuery(MeetingQueries.GET_MEETING_STATUS, {
 		skip: !id,
@@ -22,11 +25,16 @@ export default function CheckUserStatus({children}: { children: React.ReactNode}
 		nextFetchPolicy: "cache-first",  
 	});
 	const status: Status = data?.getGroupStatus
+	const {find} = useOtherMeetingFind()
 	const [isLoading, setIsLoading] = useState(true)
 	useEffect(() => {
 		if (!id || !status) {
 			setIsLoading(false);
 			return;
+		}
+		if(!find){
+			navigate(`/meetings/${status.groupId}`)
+			setEnteringMeeting(status?.groupName || "")
 		}
 		setMeetingId(status.groupId || "")
 		if(status.hasGroup){
@@ -41,7 +49,7 @@ export default function CheckUserStatus({children}: { children: React.ReactNode}
 			}
 		}
 		setIsLoading(false)
-	}, [id, status, navigate, setMeetingId, setEnteringMeeting, setSendRequest, setFree]);
+	}, [id, status, params, find, navigate, setMeetingId, setEnteringMeeting, setSendRequest, setFree]);
 	 if (isLoading) return null;
 	 return children;
 }
